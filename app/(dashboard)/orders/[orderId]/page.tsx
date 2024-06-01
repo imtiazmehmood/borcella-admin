e2d57b00435011ba027b72cc907fb2364fa513ae
Imtiaz Mahmood <imtiazmehmood26@gmail.com>
2024-06-01 13:15:23 +0500

    setup order screens

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -1,11 +1,14 @@
-import { DataTable } from "@/components/custom ui/DataTable"
-import { columns } from "@/components/orderItems/OrderItemsColums"
+import { DataTable } from "@/components/custom ui/DataTable";
+import { columns } from "@/components/orderItems/OrderItemsColums";
 
-const OrderDetails = async ({ params }: { params: { orderId: string }}) => {
-  const res = await fetch(`${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`)
-  const { orderDetails, customer } = await res.json()
+const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
+  const res = await fetch(
+    `${process.env.NEXT_BASE_URL}/api/orders/${params.orderId}`
+  );
+  const { orderDetails, customer } = await res.json();
 
-  const { street, city, state, postalCode, country } = orderDetails.shippingAddress
+  const { street, city, state, postalCode, country } =
+    orderDetails.shippingAddress;
 
   return (
     <div className="flex flex-col p-10 gap-5">
@@ -16,17 +19,26 @@ const OrderDetails = async ({ params }: { params: { orderId: string }}) => {
         Customer name: <span className="text-base-medium">{customer.name}</span>
       </p>
       <p className="text-base-bold">
-        Shipping address: <span className="text-base-medium">{street}, {city}, {state}, {postalCode}, {country}</span>
+        Shipping address:{" "}
+        <span className="text-base-medium">
+          {street}, {city}, {state}, {postalCode}, {country}
+        </span>
       </p>
       <p className="text-base-bold">
-        Total Paid: <span className="text-base-medium">${orderDetails.totalAmount}</span>
+        Total Paid:{" "}
+        <span className="text-base-medium">${orderDetails.totalAmount}</span>
       </p>
       <p className="text-base-bold">
-        Shipping rate ID: <span className="text-base-medium">{orderDetails.shippingRate}</span>
+        Shipping rate ID:{" "}
+        <span className="text-base-medium">{orderDetails.shippingRate}</span>
       </p>
-      <DataTable columns={columns} data={orderDetails.products} searchKey="product"/>
+      <DataTable
+        columns={columns}
+        data={orderDetails.products}
+        searchKey="product"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default OrderDetails
+export default OrderDetails;
